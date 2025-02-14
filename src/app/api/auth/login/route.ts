184ab6752ac1f924f7b28a7956772d2fe3a0ec72
theashuM0 <ashutosh@m0.ventures>
2025-02-14 20:54:29 +0530

    fixed

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -89,7 +89,7 @@ export async function POST(req: Request) {
 
     response.cookies.set("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: process.env.NODE_ENV === "development",
       sameSite: "strict",
       path: "/",
       maxAge: 60 * 60 * 24 * 7,
